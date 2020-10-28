fb299e44110c670c924779b7d37dee583bea5bff
Anmol-Gill <67686068+Anmol-Gill@users.noreply.github.com>
2020-10-27 20:29:11 -0700

    Assignment 5

--- Homework5/src/ListManipulator.java
+++ Homework5/src/ListManipulator.java
@@ -2,14 +2,44 @@ import java.util.*;
 public class ListManipulator {
 	
 	ArrayList<Integer> sort(ArrayList<Integer> myLst, Boolean ascending){
+		if (ascending == true) {
+			myLst.sort(new Comparator<Integer>() 
+			{
+				@Override
+				public int compare(Integer o1, Integer o2) {
+					return o1.compareTo(o2);
+				}
+			});
+		}
+		else {
+			myLst.sort(new Comparator<Integer>() 
+			{
+				@Override
+				public int compare(Integer o1, Integer o2) {
+					return o2.compareTo(o1);
+				}
+			});
+		}
 		return myLst;
 	}
 
 	ArrayList<Integer> swapLargestSmallest(ArrayList<Integer> myLst) {
-		return myLst;	
+		
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			int x = scan.nextInt();
+			myLst.add(x);
+		}
+		
+		myLst.remove(0);
+		myLst.add(0, myLst.lastIndexOf(myLst));
+	    return myLst;
 	}
 	
 	void table(ArrayList<Integer> myLst) {
-		
+		System.out.println("Number\tTrequency");
+		for(int i = 0; i<myLst.size();i++) {
+			System.out.println(myLst.get(i)+"\t"+Collections.frequency(myLst, myLst.get(i)));
+		}
 	}
 }
--- Homework5/src/Student.java
+++ Homework5/src/Student.java
@@ -1,7 +1,11 @@
-import java.util.Comparator;
+import java.util.*;
 import java.util.LinkedList;
-public class Student{
 
+public class Student extends Course{
+
+	public Student(String courseName, String courseDescrip, String dept, String time, String weekday) {
+		super(courseName, courseDescrip, dept, time, weekday);
+	}
 
 	private String firstN;
 	private String lastN;
@@ -55,20 +59,66 @@ public class Student{
 	}
 	
 	void addCourse(){
-		
+		Course x = new Course(this.getCourseName(), this.getCourseDescription(), this.getDept(), this.getTime(), this.getWeekday());
+		courses.add(x);
 	}
 	
-	private String getCourseName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	void removeCourse() {
-		
+		Course x = new Course(this.getCourseName(), this.getCourseDescription(), this.getDept(), this.getTime(), this.getWeekday());
+		courses.remove(x);
 	}
 	
-	void sortCourse() {
-		
-	}
-	
-	
+	void sortCourse(boolean x, String str) {
+		if(str.equalsIgnoreCase("Course name")) 
+		{
+			courses.sort(new Comparator<Course>() 
+			{
+				@Override
+				public int compare(Course c1, Course c2) 
+				{
+					return c1.getCourseName().compareTo(c2.getCourseName());
+				}
+			});
+		}
+		else if (str.equalsIgnoreCase("Course description")) {
+			courses.sort(new Comparator<Course>() 
+			{
+				@Override
+				public int compare(Course c1, Course c2) 
+				{
+					return c1.getCourseDescription().compareTo(c2.getCourseDescription());
+				}
+			});
+		}
+		else if (str.equalsIgnoreCase("Department")) {
+			courses.sort(new Comparator<Course>() 
+			{
+				@Override
+				public int compare(Course c1, Course c2) 
+				{
+					return c1.getDept().compareTo(c2.getDept());
+				}
+			});
+		}
+		else if (str.equalsIgnoreCase("Time")) {
+			courses.sort(new Comparator<Course>() 
+			{
+				@Override
+				public int compare(Course c1, Course c2) 
+				{
+					return c1.getTime().compareTo(c2.getTime());
+				}
+			});
+		}
+		else if (str.equalsIgnoreCase("Weekday")) {
+			courses.sort(new Comparator<Course>() 
+			{
+				@Override
+				public int compare(Course c1, Course c2) 
+				{
+					return c1.getWeekday().compareTo(c2.getWeekday());
+				}
+			});
+		}
+	}	
 }
